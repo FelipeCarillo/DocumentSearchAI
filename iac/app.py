cad341d7859f4ac65732a022613273a1c0e8d473
FelipeCarillo <felipecarillo@outlook.com>
2024-06-16 22:23:07 -0300

    Refactor environment variable retrieval in app.py for better readability and consistency

--- iac/app.py
+++ iac/app.py
@@ -10,16 +10,16 @@ dotenv.load_dotenv()
 
 app = cdk.App()
 
-AWS_ACCOUNT_ID = os.environ.get("AWS_ACCOUNT_ID")
-AWS_REGION = os.environ.get("AWS_REGION")
+AWS_ACCOUNT_ID = os.environ["AWS_ACCOUNT_ID"]
+AWS_REGION = os.environ["AWS_REGION"]
 
 ENVIROMMENT = {
-    "STACK_NAME": os.environ.get("STACK_NAME"),
-    "STAGE": os.environ.get("STAGE"),
-    "AWS_BUCKET_NAME": os.environ.get("AWS_BUCKET_NAME"),
-    "ES_API_KEY": os.environ.get("ES_API_KEY"),
-    "ES_CLOUD_ID": os.environ.get("ES_CLOUD_ID"),
-    "ES_INDEX_NAME": os.environ.get("ES_INDEX_NAME"),
+    "STACK_NAME": os.environ["STACK_NAME"],
+    "STAGE": os.environ["STAGE"],
+    "BUCKET_NAME": os.environ["BUCKET_NAME"],
+    "ES_API_KEY": os.environ["ES_API_KEY"],
+    "ES_CLOUD_ID": os.environ["ES_CLOUD_ID"],
+    "ES_INDEX_NAME": os.environ["ES_INDEX_NAME"],
 }
 
 IacStack(
