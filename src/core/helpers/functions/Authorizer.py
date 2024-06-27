d1fef2ff4800c775cd6890538c1d437cf4a806f4
FelipeCarillo <felipecarillo@outlook.com>
2024-06-27 18:10:04 -0300

    Refactor Authorizer to raise Unauthorized exception with specific message

--- src/core/helpers/functions/Authorizer.py
+++ src/core/helpers/functions/Authorizer.py
@@ -1,16 +1,21 @@
 import os
 import jwt
 
-from src.core.helpers.http.http import Unauthorized
+from src.core.helpers.http.http_codes import Unauthorized
+from src.core.helpers.exeptions.exceptions import UnauthorizedAccess
 
 
 class Authorizer:
     def __init__(self):
         self.secret = os.environ.get("JWT_SECRET")
 
-    def authorize(self, token: str):
+    def authorize(self, token: str) -> bytes:
         """
         Authorizes the request by validating the JWT token.
+        args:
+            token: str: JWT token
+        return:
+            bytes: decoded token
         """
         try:
 
@@ -20,4 +25,5 @@ class Authorizer:
             return decoded
 
         except Exception as e:
-            raise Unauthorized(message="Token expired or invalid")
+            # Raise an exception if the token is invalid or expired
+            raise UnauthorizedAccess("Token is invalid or expired.")
